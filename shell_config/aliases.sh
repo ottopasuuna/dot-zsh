c3ac7ae5dac894e5b9377f5b57e41841856b1961
Carl Hofmeister <hofmeister.carl@gmail.com>
2021-10-05 20:38:43 -0600

    Get rid of OMZ git plugin, define own aliases

--- shell_config/aliases.sh
+++ shell_config/aliases.sh
@@ -19,11 +19,26 @@ alias g++="g++ -Wall -pedantic"
 alias makeandroid="./gradlew assembleDebug && adb install -r app/build/outputs/apk/app-debug.apk"
 alias upass="sudo umount ~/.password-store"
 alias modelsim="/opt/altera/16.0/modelsim_ase/bin/vsim"
-alias gl="git log --format=\"%C(auto)%h %d %Creset%s%n%Cgreen%ar %C(magenta)%an\" --graph --all"
 alias buspirate="picocom -b 115200 /dev/buspirate"
 alias arch2="sudo systemd-nspawn -b -D ~/vm/arch2 "
 alias weather="curl wttr.in"
 alias ealias="$EDITOR $SHELL_CONFIG_DIR/aliases.sh"
+
+# Git aliases
+# alias gl="git log --format=\"%C(auto)%h %d %Creset%s%n%Cgreen%ar %C(magenta)%an\" --graph --all"
+alias gst="git status"
+alias gd="git diff"
+alias gca="git commit -a"
+alias gcam="git commit -am"
+alias gp="git push"
+# From OMZ git plugin:
+alias glg='git log --stat'
+alias glo='git log --oneline --decorate'
+alias glod='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'\'' --date=short'
+alias glog='git log --oneline --decorate --graph'
+alias gloga='git log --oneline --decorate --graph --all'
+alias glol='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'\'
+alias glola='git log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'\'' --all'
 
 alias mcli="make clean ui libs"
 
--- zshrc
+++ zshrc
@@ -38,7 +38,6 @@ zinit ice wait lucid
 zinit light zdharma/fast-syntax-highlighting
 # zinit load "zsh-users/zsh-syntax-highloading"
 zinit load zsh-users/zsh-completions
-zinit snippet OMZ::plugins/git/git.plugin.zsh
 zinit snippet OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh
 zinit load "esc/conda-zsh-completion"
 zinit load "supercrabtree/k"
